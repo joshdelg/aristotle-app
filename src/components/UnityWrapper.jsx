5ce9041c7c267fc722eba5c1aa8a8d97f602030a
Josh Delgadillo <delgadillo.i.joshua@gmail.com>
2021-01-17 17:42:50 -0800

    Attempted unity integration, now using iframe

--- src/components/UnityWrapper.jsx
+++ src/components/UnityWrapper.jsx
@@ -1,9 +1,19 @@
 import { Box, Heading } from "@chakra-ui/react";
+import Unity, { UnityContext } from "react-unity-webgl";
 
 function UnityWrapper() {
+
+    let unityContext = new UnityContext({
+        loaderUrl: "./unity/Build/AristotleBuild.loader.js",
+        dataUrl: "./unity/Build/AristotleBuild.data.br",
+        frameworkUrl: "./unity/Build/AristotleBuild.framework.js.br",
+        codeUrl: "./unity/Build/AristotleBuild.wasm.br"
+    });
+
     return (
         <Box p={4} h="100%" borderWidth="1px" borderRadius="lg" textAlign="center">
-            <Heading size="xl">Unity Game!</Heading>
+            {/*<Unity unityContext={unityContext} />*/}
+            <iframe width="800px" height="800px" title="hi" src="https://unity-game-proj.herokuapp.com/"></iframe>
         </Box>
     )
 };
